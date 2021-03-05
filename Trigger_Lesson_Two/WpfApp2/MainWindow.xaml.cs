e725a6806fb47b78d6b79e8a4773541995f0c0ab
Rasim <allen.wallker.00@bk.ru>
2021-03-05 23:04:49 +0400

    First Commit

---
+++ Trigger_Lesson_Two/WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using PropertyChanged;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    [AddINotifyPropertyChangedInterface]
+    public partial class MainWindow : Window
+    {
+        //public string MyProp { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+            //ListBox1.Items.Add("Zero");
+            // = "zero";
+            //Pic.Source = new BitmapImage(new Uri(@"C:\Users\User\Desktop\Pic\Cat.png"));
+        }
+
+
+    }
+}
