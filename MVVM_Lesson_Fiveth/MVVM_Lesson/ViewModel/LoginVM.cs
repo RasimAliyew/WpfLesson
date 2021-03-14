e743171fb884a0b380e15dc8839860225f7b6142
Rasim <allen.wallker.00@bk.ru>
2021-03-14 19:58:55 +0400

    Test

---
+++ MVVM_Lesson_Fiveth/MVVM_Lesson/ViewModel/LoginVM.cs
@@ -0,0 +1,28 @@
+﻿using MVVM_Lesson.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MVVM_Lesson.ViewModel
+{
+    class LoginVM : ViewModelBase
+    {
+        //public RelayCommand command { get; set; }
+        //RegisterButton
+        public Command RegisterButton { get; set; }
+        
+        public LoginVM()
+        {
+            RegisterButton = new Command(x => GoToRegisterUC());
+        }
+        public void GoToRegisterUC()
+        {
+            //CurrentVM = new RegisterVM();
+        }
+
+
+    }
+}
---
+++ MVVM_Lesson_Fiveth/MVVM_Lesson/ViewModel/MainVM.cs
@@ -0,0 +1,45 @@
+﻿using MVVM_Lesson.Commands;
+using MVVM_Lesson.Model;
+using PropertyChanged;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MVVM_Lesson.ViewModel
+{
+    //[AddINotifyPropertyChangedInterface]
+    class MainVM : ViewModelBase
+    {
+        private ViewModelBase currentVM;
+
+        public ViewModelBase CurrentVM { get => currentVM; set { Set(ref currentVM, value); } }
+        public Command RegisterButton { get; set; }
+        public Command LoginButton { get; set; }
+        public MainVM()
+        {
+
+            //Action<object> action;
+            //action = Test;
+            //CurrentVM = new LoginVM();
+            RegisterButton = new Command(x => GoToRegisterUC());
+            LoginButton = new Command(x => GoToLoginUC());
+            //ObservableCollection<UserInformation> information;
+            // Test();
+        }
+        public void GoToRegisterUC()
+        {
+            CurrentVM = new RegisterVM();
+            //MessageBox.Show("It's working");
+        }
+        public void GoToLoginUC()
+        {
+            CurrentVM = new LoginVM();
+            
+        }
+   
+    }
+}
